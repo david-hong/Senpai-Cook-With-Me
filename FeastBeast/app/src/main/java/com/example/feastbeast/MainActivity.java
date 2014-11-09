12c142fc30e19b8c61e2dabbbde36a364ecb6024
David Hong <d22hong@uwaterloo.ca>
2014-11-08 20:28:13 -0800

    added directions linked list and supported more websites

---
+++ FeastBeast/app/src/main/java/com/example/feastbeast/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.feastbeast;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class MainActivity extends Activity {
+
+    private TextView respText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText edtUrl = (EditText) findViewById(R.id.edtURL);
+        Button btnGo = (Button) findViewById(R.id.btnGo);
+        respText = (TextView) findViewById(R.id.edtResp);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String siteUrl = edtUrl.getText().toString().toLowerCase();
+                if (! (siteUrl.substring(0, Math.min(siteUrl.length(), 7)).equals("http://")))
+                    siteUrl = "http://" + siteUrl;
+                ( new ParseURL() ).execute(new String[]{siteUrl});
+            }
+        });
+    }
+
+    public void newRec(View view){
+        Intent intent = new Intent(this,NewRecipe.class);
+        startActivity(intent);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    class Node{
+        String value ="";
+        Node next = null;
+        Node prev = null;
+
+        public Node(String str){
+            this.value = str;
+        }
+    }
+
+
+    class Directions{
+        Node current = null;
+
+        public Directions(){
+            this.current = new Node("");
+        }
+
+        public Directions(String str){
+            this.current = new Node(str);
+        }
+
+        protected void add(String str){
+            Node holder = this.current;
+            while(holder.next != null)
+                holder = holder.next;
+            holder.next = new Node(str);
+            holder.next.prev = holder;
+        }
+
+        public String toString(){
+            String str= "";
+            Node holder = this.current;
+            while(holder != null){
+                str = str + holder.value;
+                holder = holder.next;
+            }
+            return str;
+        }
+    }
+
+    protected class ParseURL extends AsyncTask<String, Void, String> {
+
+        String classFind = "li";
+        Directions directions = new Directions();
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuffer buffer = new StringBuffer();
+            try {
+                Log.d("JSwa", "Connecting to ["+strings[0]+"]");
+                Document doc  = Jsoup.connect(strings[0]).get();
+                Log.d("JSwa", "Connected to ["+strings[0]+"]");
+                // Get document (HTML page) title
+                String title = doc.title();
+                Log.d("JSwA", "Title ["+title+"]");
+                buffer.append("Title: " + title + "\r\n");
+
+                if(strings[0].substring(0, Math.min(strings[0].length(), 26)).equals("http://www.foodnetwork.ca/"))
+                    classFind = ".recipeInstructions";
+                else if(strings[0].substring(0, Math.min(strings[0].length(), 22)).equals("http://allrecipes.com/"))
+                    classFind = ".directions li";
+                else if(strings[0].substring(0, Math.min(strings[0].length(), 20)).equals("http://www.food.com/"))
+                    classFind = ".instructions li";
+                else {
+                    buffer.setLength(0);
+                    buffer.append("Error only accepts recipes from http://www.foodnetwork.ca/, http://allrecipes.com/, http://www.food.com/");
+                }
+
+                Elements topicList = doc.select(classFind);
+                if(!buffer.toString().equals("Error only accepts recipes from http://www.foodnetwork.ca/, http://allrecipes.com/, http://www.food.com/")){
+                buffer.append("Directions \r\n");
+                  for (Element topic : topicList) {
+                      String data = topic.text();
+                      directions.add(data);
+                      buffer.append(data + "\r\n");
+                  }
+                }
+            }
+            catch(Throwable t) {
+                t.printStackTrace();
+            }
+            return directions.toString();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            respText.setText(s);
+        }
+    }
+}
